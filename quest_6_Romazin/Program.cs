7d5ea118f26a54f8ed39c7102050b2d975881ab6
Denis Romazin <romazin777@gmail.com>
2022-08-20 15:19:15 +0500

    Задача №6

---
+++ quest_6_Romazin/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    if (numb % 2 == 0)
+    {
+        Console.WriteLine("Число " + numb + " четное");
+    }
+    else
+    {
+        Console.WriteLine("Число " + numb + " нечетное");
+    }
