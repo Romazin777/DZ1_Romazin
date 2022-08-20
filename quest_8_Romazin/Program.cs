7c534e20e9781df33725a21518473a93f482b6a3
Denis Romazin <romazin777@gmail.com>
2022-08-20 15:26:56 +0500

    Задача №8

---
+++ quest_8_Romazin/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+    int numb = Convert.ToInt32(Console.ReadLine());
+        if (numb < 2 )
+    {
+        Console.WriteLine("Чётных чисел до " + numb + " нет");
+    }
+    for (int numbX = 2; numbX <= numb; numbX = numbX + 2)
+    {
+        Console.Write("{0}, ", + numbX);
+    }
